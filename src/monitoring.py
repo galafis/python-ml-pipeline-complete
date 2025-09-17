10f2822c1233ed15a194c0bf641f26d478966028
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2025-09-17 19:30:08 -0300

    fix: adiciona linhas em branco necessárias para conformidade PEP8 em monitoring.pyUpdate monitoring.py

--- src/monitoring.py
+++ src/monitoring.py
@@ -2,6 +2,7 @@
 from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset
 
+
 def generate_drift_report(reference, current):
     report = Report(metrics=[DataDriftPreset()])
     report.run(reference_data=reference, current_data=current)
